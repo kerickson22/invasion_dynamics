723ea0dbdff86b6ad9d9b315d3c24b5c253635bb
Kelley Erickson <kerickson22@gmail.com>
2018-08-06 16:58:33 -0500

    Update to loop through all species in the species list

--- Invasion Dynamics for Missouri species.R
+++ Invasion Dynamics for Missouri species.R
@@ -9,107 +9,79 @@ memory.limit(memory.limit() *2^30)
 
 #A selection of invasive species from the MDC website (https://nature.mdc.mo.gov/status/invasive)
 # that are possibly dispersed by animals 
-speciesList<-c("Elaeagnus umbellata", "Lonicera maackii", "Pyrus calleryana", "Rhomnus cathartica",
-                "Nandina domestica", "Lonicera japonica", "Rusa multiflora", "Elaeagnus angustifolia",
+speciesList<-c("Schinus terebinthifolia", "Elaeagnus umbellata", "Lonicera maackii", "Pyrus calleryana", "Rhamnus cathartica",
+                "Nandina domestica", "Lonicera japonica", "Rosa multiflora", "Elaeagnus angustifolia",
                 "Euonymus fortunei", "Duchesnea indica")
 
-E_umbellata <- occ_search(scientificName = "Elaeagnus umbellata", return =  "data", limit=20000)
-gbifmap(E_umbellata)
-save(E_umbellata, file='00 Original data downloaded from GBIF.rData')
-#There are 7037 observations of 160 variables. 
+dat2<-data.frame()
+#speciesList<-c("Elaeagnus umbellata", "Lonicera maackii")
+for (i in speciesList) {
+  cat ("starting: ", i, "\n")
+  dat <- occ_search(scientificName = i, return = "data", limit=20000)
+  dat<- data.frame( 
+    idNum = dat$gbifID,
+    rawSpecies=dat$scientificName,
+    species = i,
+    longitude=dat$decimalLongitude,
+    latitude=dat$decimalLatitude,
+    coordUncer=dat$coordinateUncertaintyInMeters,
+    recordType=dat$basisOfRecord,
+    country=dat$countryCode,
+    state=dat$stateProvince,
+    county=dat$county,
+    locality=dat$locality,
+    date=dat$eventDate,
+    year=dat$year,
+    institution=dat$institutionCode,
+    identifiedBy=dat$identifiedBy
+  )
+  dat2 <- rbind(dat, dat2)
+  cat("finished with: ", i,"\n");
+}
+
+save(dat2, file='00 Original data downloaded from GBIF.rData')
+
+#With just two species there are 9689 observations
+
+### Remove observations that are missing coordinates: 
+dat2_missingCoords <- dat2[is.na(dat2$longitude), ]
+dat2 <- dat2[!is.na(dat2$longitude), ]
+#Removing observations that are missing a longitude coordinate gets rid of 4959
+# records (more than half of the records!)
+
+save(dat2, file='02 Remove missing coordinates.rData')
+save(dat2_missingCoords, file= '02b Records that are missing coordinates.rData')
+
+#### Remove observations that are missing year
+dat2_missingYear <- dat2[is.na(dat2$year), ]
+dat2 <- dat2[!is.na(dat2$year), ]
+
+save(dat2, file = '03 Remove datapoints missing year.rData')
+save(dat2_missingYear, file='03b Records that have coords but are missing year.rData')
+
+### Remove coordinates that are not in the US? 
+dat2_missingCountry <- subset (dat2, (dat2$country == 'none' | dat2$country == 'ZZ') )
+dat2 <- dat2[dat2$country == "US", ]
 
 
-E_umbellata<- data.frame( 
-  idNum = E_umbellata$gbifID,
-  rawSpecies=E_umbellata$scientificName,
-  longitude=E_umbellata$decimalLongitude,
-  latitude=E_umbellata$decimalLatitude,
-  coordUncer=E_umbellata$coordinateUncertaintyInMeters,
-  recordType=E_umbellata$basisOfRecord,
-  country=E_umbellata$countryCode,
-  state=E_umbellata$stateProvince,
-  county=E_umbellata$county,
-  locality=E_umbellata$locality,
-  date=E_umbellata$eventDate,
-  year=E_umbellata$year,
-  institution=E_umbellata$institutionCode,
-  identifiedBy=E_umbellata$identifiedBy
+
+save(dat2, file='04 Remove data points not in US.rData')
+save(dat2_missingCountry, file = '04b Records that have coords and year but are missing country code.rData')
+
+### Draw histograms of the number of records by year for each species 
+
+summary(dat2$year)
+for (i in speciesList) {
+    species_i <- subset(dat2, dat2$species == i)
+    hist(species_i$year,
+    xlim=c(1873, 2018),
+    xlab='Collection year',
+    ylab='Number of records',
+    main=bquote("Species: " ~ .(i) )
 )
-#Now there are 7037 observations of 14 variables. 
-save(E_umbellata,  file='01 Saved as dataframe.rData')
+}
 
-
-#Remove observations that are missing coordinates 
-E_umbellata_missingCoords<-E_umbellata[is.na(E_umbellata$longitude), ]
-#There are 3860 observations that are missing coordinates (over half!)
-save(E_umbellata_missingCoords, file='02b Observations missing coordinates.rData')
-summary(E_umbellata_missingCoords$country)
-# Of these 3860 observations that are missing coordinates, 902 have no country 'none'
-# 244 records are listed as US
-summary(E_umbellata_missingCoords$state)
-# 21 recrods from Massachusetts
-# 21 records from South Carolina
-# 15 records from New York
-# 13 records from Michigan
-# 13 records from Missouri
-# 12 records from Florida
-# 11 records from North Carolina
-# 9 records from Hawaii 
-# 6 records from Virginia 
-# 5 records from Georgia 
-# 5 records from Oregon
-# 4 records from Connecticut
-# 4 records from New Hampshire
-# 4 records from Pennsylvania
-# 3 records from Louisiana
-# 3 records from Maine
-# 3 records from Rhode Island
-# # 3 records from Wisconsin
-# and 95 records from (Other)
-
-summary(E_umbellata_missingCoords$county)
-
-
-
-
-
-
-E_umbellata<-E_umbellata[!is.na(E_umbellata$longitude), ]
-save(E_umbellata, file='02 Remove missing coordinates.rData')
-
-#Remove observations without a year
-E_umbellata_coordsMissingYear<-E_umbellata[is.na(E_umbellata$year), ]
-#There are 218 observations that have coordinates but are missing a year 
-save(E_umbellata_coordsMissingYear, file = '03b Observations with coordinates missing year.rData')
-
-
-E_umbellata<- E_umbellata[!is.na(E_umbellata$year), ]
-save(E_umbellata, file='03 Remove data points that are missing year.rData')
-
-#Remove observations that are not in the US
-#Is this step removing valid points?
-E_umbellata_missingCountry<-E_umbellata[is.na(E_umbellata$country), ]
-#There are no observations that lack a country 
-
-
-E_umbellata <- E_umbellata[E_umbellata$country=="US", ]
-save(E_umbellata, file='04 Remove data points not in US.rData')
-
-#Remove coordinates with longitude outside of US 
-E_umbellata <- E_umbellata[E_umbellata$longitude < -40, ]
-save(E_umbellata, file='05 Remove data points with longitude outside of US.rData')
-
-hist(E_umbellata$year,
-     xlab='Collection year',
-     ylab='Number of records',
-     main=''
-)
-
-
-years<-unique(E_umbellata$year)
-##Rearrange in ascending order 
-years2<-years[order(years)]
-
+### Make yearly plots for each species
 
 
 #Load in shapefile of United States (edited in ArcGIS)
@@ -117,15 +89,6 @@ unitedStates <- rgdal::readOGR (
   './United States Shape',
   'United_states'
 )
-
-par(xpd=T)
-sp::plot(unitedStates, col='gray80', xlim=c(-120, -70), ylim=c(26, 54))
-points(E_umbellata$longitude, E_umbellata$latitude, bg='red', pch=21)
-mtext("E. umbellata", side=3, cex=2)
-
-
-#number of frames or plots
-frames <- length(years2)
 
 # function for creating file name with leading zeros
 # makes it easier to process them sequentially
@@ -143,30 +106,68 @@ rename <- function(x){
   }
 }
 
-setwd("C:/Users/kerickson/Documents/GitHub/invasion_dynamics/E_umbellata_plots")
 
-#Create the individual frames of the GIF
-for(i in 1:frames){
-  name <- rename(i)
+#par(xpd=T)
+#sp::plot(unitedStates, col='gray80', xlim=c(-120, -70), ylim=c(26, 54))
+#points(dat2$longitude, dat2$latitude, bg='red', pch=21)
+#mtext("All species, All Years", side=3, cex=2)
+
+
+for (j in speciesList) {
+  cat ("starting: ", j, "\n")
+  species_j <- subset(dat2, dat2$species == j)
+  years <- unique(species_j$year)
+  #rearrange so that years are in ascending order 
+  years2 <- years[order(years)]
   
-  #saves the plot as a .png file in the working directory
-  png(name)
-  par(xpd=T)
-  dat<-subset(E_umbellata, (E_umbellata$year <=years2[i]))
-  sp::plot(unitedStates, col='gray80', xlim=c(-120, -70), ylim=c(26, 54))
-  points(dat$longitude, dat$latitude,
-         bg='red', pch=21)
-  mtext(bquote("Species: E. umbellata \n Year" ~ .(years2[i])), side=3, cex=2)
-  #text(-100, 54, bquote("Year:" ~ .(years2[i])), cex=2)
-  dev.off()
+  directory<-paste0('./', bquote(.(j)))
+  
+  dir.create(directory, recursive=TRUE, showWarnings=FALSE)
+  
+  #number of frames or plots
+  frames <- length(years2)
+  
+  
+  setwd(directory)
+  #Create the individual frames of the GIF
+  for(i in 1:frames){
+    name <- rename(i)
+    
+    #saves the plot as a .png file in the working directory
+    png(name)
+    par(xpd=T)
+    dat3<-subset(species_j, (species_j$year <=years2[i]))
+    sp::plot(unitedStates, col='gray80', xlim=c(-120, -70), ylim=c(26, 54))
+    points(dat3$longitude, dat3$latitude,
+           bg='red', pch=21)
+    mtext(bquote("Species:" ~  .(j)), side=3, cex=2)
+    mtext(bquote("\n Year:" ~ .(years2[i])), side=1, cex=2)
+    #text(-100, 54, bquote("Year:" ~ .(years2[i])), cex=2)
+    dev.off()
+  }
+  
+  fileName<-paste0(bquote(.(j)), ".gif")
+  #Use the purr and magick packages to combine the png files into a GIF
+  # This part of the code comes from a post by Ryan Peek on how to make GIF 
+  # files in R: https://ryanpeek.github.io/2016-10-19-animated-gif_maps_in_R/ 
+  list.files(path = "./", pattern = "*.png", full.names = T) %>% 
+    map(image_read) %>% # reads each path file
+    image_join() %>% # joins image
+    image_animate(fps=2) %>% # animates, can opt for number of loops
+    image_write(fileName) # write to current dir
+  setwd("C:/Users/kerickson/Documents/GitHub/invasion_dynamics")
+  
 }
 
-#Use the purr and magick packages to combine the png files into a GIF
-# This part of the code comes from a post by Ryan Peek on how to make GIF 
-# files in R: https://ryanpeek.github.io/2016-10-19-animated-gif_maps_in_R/ 
-list.files(path = "./", pattern = "*.png", full.names = T) %>% 
-  map(image_read) %>% # reads each path file
-  image_join() %>% # joins image
-  image_animate(fps=2) %>% # animates, can opt for number of loops
-  image_write("E_umbellata_invasion.gif") # write to current dir
+
+
+
+
+
+
+
+
+
+
+
 
