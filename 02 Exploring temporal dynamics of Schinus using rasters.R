e8b3742fdd2f458a8445f539d13fdd5fc111ddaf
kerickson22 <kerickson22@gmail.com>
2018-09-24 14:57:47 -0500

    Add tracheophyte data

--- 02 Exploring temporal dynamics of Schinus using rasters.R
+++ 02 Exploring temporal dynamics of Schinus using rasters.R
@@ -68,4 +68,47 @@ thing <- sp::spTransform(florida, getCRS("albersNA", TRUE))
 
 #Read in records of all Tracheophytes in Florida from GBIF: 
 tracheophyta <-read.delim("./Tracheophyta/occurrence.txt")
-tracheophyta_XY <- SpatialPoints(cbind(tracheophyta$decimalLongitude, tracheophyta$decimalLatitude), getCRS('wgs84', TRUE))
+tracheophyta2 <- read.csv("./Tracheophyta/occurrence2.csv", header=T)
+#To create tracheophyta2, I opened the txt file in Excel and saved it as a csv. 
+# It seems like the csv file has retained more of the records without introducing errors 
+
+
+
+#Check: 
+summary(tracheophyta2$stateProvince)
+#When I run this, there are 128,525 with state Florida, 1 with state florida, and 69 with random strings of characters...
+# This is data that I downloaded from GBIF directly (I did not use rgbif)
+
+tracheophyta2 <- subset(tracheophyta2, tracheophyta2$stateProvince == "florida" | tracheophyta2$stateProvince=="Florida")
+
+tracheophyta2$stateProvince <- gsub(tracheophyta2$stateProvince, pattern='florida', replacement='Florida', fixed=T)
+tracheophyta2$stateProvince <- factor(tracheophyta2$stateProvince)
+
+coords_tracheophyta <- subset(tracheophyta2, tracheophyta2$decimalLatitude != '')
+coords_tracheophyta <- subset(coords_tracheophyta, coords_tracheophyta$decimalLongitude != '')
+
+tracheophyta_XY <- SpatialPoints(cbind(coords_tracheophyta$decimalLongitude, coords_tracheophyta$decimalLatitude), getCRS('wgs84', TRUE))
+
+
+
+
+#Add a column for tracheophytes to the florida shapefile 
+x <- data.frame(numTracheo=rep(0, nrow(florida)))
+florida@data <- omnibus::insertCol(x, into=florida@data, at='numAnacard', before=FALSE)
+
+
+# intersect coordinates with counties
+
+
+TracheoInFlorida <- over(tracheophyta_XY, florida) 
+TracheoInFlorida <- TracheoInFlorida[!is.na(TracheoInFlorida$NAME_2), ]
+
+
+# find number of records in each county (do by county)
+#For all Tracheophyta
+for (i in 1:nrow(TracheoInFlorida)) {
+  
+  thisRow <- which(florida$NAME_1 == TracheoInFlorida$NAME_1[i] & florida$NAME_2 == TracheoInFlorida$NAME_2[i])
+  florida@data$numTracheo[thisRow] <- florida@data$numTracheo[thisRow] + 1
+  
+}
